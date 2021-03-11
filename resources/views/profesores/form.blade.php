0c3088b85bd7762672db79bcf055e390ee1ad6d4
Fabrizio Piminchumo <201814058@uns.edu.pe>
2021-03-11 10:17:40 -0500

    Update form.blade.php

--- resources/views/profesores/form.blade.php
+++ resources/views/profesores/form.blade.php
@@ -7,8 +7,50 @@
     <title>Document</title>
 </head>
 <body>
-   
+
 <h1>{{$modo}} Profesor</h1>
+
+<form>
+    <div class="form-group">
+        <label for="nombres">Nombres:</label>
+        <input type="text" id="nombres" name="nombres" class="form-control" value="{{old('nombres',isset($profesor->user->name)?$profesor->user->name:'')}}" id="nombres" required>
+    </div>
+    <div class="form-group">
+        <label for="nombres">Nombres:</label>
+        <input type="text" name="apellidos" class="form-control" value="{{old('apellidos',isset($profesor->user->last_name)?$profesor->user->last_name:'')}}"id="apellidos" required>
+    </div>
+    <div class="form-group">
+        <label for="nombres">Nombres:</label>
+        <input type="text" name="especialidad" class="form-control" value="{{old('especialidad',isset($profesor->especialidad)?$profesor->especialidad:'')}}"id="especialidad" required>
+    </div>
+    <div class="form-group">
+        <label for="nombres">Cursos:</label>
+        <select name="curso_id" id="curso_id" class="form-control">
+            @foreach ($arreDataCursos as $curso )
+                <option value="{{$curso->id}}">{{$curso->curso}}</option>
+            @endforeach
+        </select>
+    </div>
+    <input type="submit" value="{{$modo}} Profesor" class="btn btn-success">
+    <a href="{{url('/profesor/')}}" class="btn btn-warning">Regresar </a>
+</form>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
 <div class="form-group">
 <label for="nombres">Nombres:</label>
 <input type="text" name="nombres"  value="{{isset($profesor->user->name)?$profesor->user->name:''}}" id="nombres">
@@ -28,11 +70,11 @@
         <option value="{{$curso->id}}">{{$curso->curso}}</option>
         @endforeach
      </select>
-    </div>
+</div>
 <div class="form-group">
 <input type="submit" value="{{$modo}} Profesor" class="btn btn-success">
 <a href="{{url('/profesor/')}}" class="btn btn-warning">Regresar </a>
 </div>
-    
+
 </body>
 </html>
