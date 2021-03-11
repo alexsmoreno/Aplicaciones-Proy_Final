182e4597abc6c5666ebdb4beee651c70cb4011b3
Fabrizio Piminchumo <201814058@uns.edu.pe>
2021-03-11 10:40:57 -0500

    up

--- resources/views/alumnos/form.blade.php
+++ resources/views/alumnos/form.blade.php
@@ -5,27 +5,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
     <title>Document</title>
 </head>
 <body>
 
 <h1>{{$modo}} Alumno</h1>
+
+
 <div class="form-group">
-<label for="dni">DNI</label>
-<input type="text" name="dni"  value="{{isset($alumno->DNI)?$alumno->DNI:''}}" id="dni">
+    <label for="nombres">DNI:</label>
+    <input type="text" id="nombres" name="dni" class="form-control" value="{{old('nombres',isset($alumno->DNI)?$alumno->DNI:'')}}" id="nombres" required>
 </div>
 <div class="form-group">
-<label for="apellidos">Apellidos</label>
-<input type="text" name="apellidos" value="{{isset($alumno->APELLIDOS)?$alumno->APELLIDOS:''}}"id="apellidos">
+    <label for="nombres">Apellidos:</label>
+    <input type="text" name="apellidos" class="form-control" value="{{old('apellidos',isset($alumno->APELLIDOS)?$alumno->APELLIDOS:'')}}" id="apellidos" required>
 </div>
 <div class="form-group">
-<label for="nombres">Nombres</label>
-<input type="text" name="nombres" value="{{isset($alumno->NOMBRES)?$alumno->NOMBRES:''}}"id="nombres">
+    <label for="nombres">Nombres:</label>
+    <input type="text" name="nombres" class="form-control" value="{{old('especialidad',isset($alumno->NOMBRES)?$alumno->NOMBRES:'')}}" id="especialidad" required>
 </div>
 <div class="form-group">
-<input type="submit" value="{{$modo}} Alumno" class="btn btn-success">
-<a href="{{url('/alumno/')}}" class="btn btn-warning">Regresar </a>
+    <input type="submit" value="{{$modo}} Alumno" class="btn btn-success">
+    <a href="{{url('/alumno/')}}" class="btn btn-warning">Regresar </a>
 </div>
+
+
 </body>
 </html>
--- resources/views/profesores/form.blade.php
+++ resources/views/profesores/form.blade.php
@@ -10,7 +10,6 @@
 
 <h1>{{$modo}} Profesor</h1>
 
-<form>
     <div class="form-group">
         <label for="nombres">Nombres:</label>
         <input type="text" id="nombres" name="nombres" class="form-control" value="{{old('nombres',isset($profesor->user->name)?$profesor->user->name:'')}}" id="nombres" required>
@@ -33,48 +32,6 @@
     </div>
     <input type="submit" value="{{$modo}} Profesor" class="btn btn-success">
     <a href="{{url('/profesor/')}}" class="btn btn-warning">Regresar </a>
-</form>
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-
-<div class="form-group">
-<label for="nombres">Nombres:</label>
-<input type="text" name="nombres"  value="{{isset($profesor->user->name)?$profesor->user->name:''}}" id="nombres">
-</div>
-<div class="form-group">
-<label for="apellidos">Apellidos:</label>
-<input type="text" name="apellidos" value="{{isset($profesor->user->last_name)?$profesor->user->last_name:''}}"id="apellidos">
-</div>
-<div class="form-group">
-<label for="especialidad">Especialidad:</label>
-<input type="text" name="especialidad" value="{{isset($profesor->especialidad)?$profesor->especialidad:''}}"id="especialidad">
-</div>
-<div class="form-group">
-    <label for="nombres">Cursos:</label>
-     <select name="curso_id" id="curso_id" class="form-control">
-        @foreach ($arreDataCursos as $curso )
-        <option value="{{$curso->id}}">{{$curso->curso}}</option>
-        @endforeach
-     </select>
-</div>
-<div class="form-group">
-<input type="submit" value="{{$modo}} Profesor" class="btn btn-success">
-<a href="{{url('/profesor/')}}" class="btn btn-warning">Regresar </a>
-</div>
 
 </body>
 </html>
