cbbbbf895938e3691ddefc1a8d63317cd3614b71
Fabrizio Piminchumo <201814058@uns.edu.pe>
2021-03-11 10:59:34 -0500

    up

--- resources/views/cursos/form.blade.php
+++ resources/views/cursos/form.blade.php
@@ -5,20 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
     <title>Editar Curso</title>
 </head>
 <body>
 
 <h1>{{$modo}} Curso</h1>
+
 <div class="form-group">
-<label for="Codigo">Codigo:</label>
-<input type="text" name="Codigo"  value="{{isset($curso->codigo)?$curso->codigo:''}}" id="Codigo">
+    <label for="Codigo">Codigo:</label>
+    <input type="text" id="Codigo" name="codigo" class="form-control" value="{{old('nombres',isset($curso->codigo)?$curso->codigo:'')}}" id="Codigo" required>
 </div>
 <div class="form-group">
-<label for="curso">Nombre Curso:</label>
-<input type="text" name="curso" value="{{isset($curso->curso)?$curso->curso:''}}"id="curso">
+    <label for="apellidos">Nombre Curso:</label>
+    <input type="text" name="curso" class="form-control" value="{{old('apellidos',isset($curso->codigo)?$curso->codigo:'')}}" id="curso" required>
 </div>
+
 <div class="form-group">
 <input type="submit" value="{{$modo}} Curso" class="btn btn-success">
 <a href="{{url('/curso/')}}" class="btn btn-warning">Regresar </a>
